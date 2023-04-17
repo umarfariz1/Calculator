5c7f53aa9c7571635b70b07b978a3923dfb03c99
umarfariz1 <58098088+umarfariz1@users.noreply.github.com>
2023-04-17 14:04:36 +0530

    update calculator codes

--- src/test/java/calculatorPackage/CalculatorProject.java
+++ src/test/java/calculatorPackage/CalculatorProject.java
@@ -1,7 +1,29 @@
 package calculatorPackage;
 
-public class CalculatorProject {
+import java.util.Scanner;
 
+public class CalculatorProject {
+public void userInterface()
+{
+	System.out.println("Select the operation below::");
+	System.out.println("1)Addition\n2)Subtraction\n3)Multiplication\n4)Division");
+	
+	Scanner sc = new Scanner(System.in);
+	int choice = sc.nextInt();
+	
+	switch (choice)
+	{
+	case 1:
+		
+	case 2:
+		
+	case 3:
+		
+	case 4:
+	}
+	
+	
+}
 	public static void main(String[] args) {
 	System.out.println("Welcome to Calculator program");
 
